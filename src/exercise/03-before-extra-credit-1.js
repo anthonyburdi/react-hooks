a8da5f0650cacf37dea21d3d4cc9d85726dd7cf3
Anthony Burdi <anthonyburdi@gmail.com>
2023-12-29 16:32:11 -0500

    Exercise 3 Extra Credit - remove name component params

--- src/exercise/03-before-extra-credit-1.js
+++ src/exercise/03-before-extra-credit-1.js
@@ -3,16 +3,11 @@
 
 import * as React from 'react'
 
-function Name() {
-  const [name, setName] = React.useState('')
+function Name({name, onNameChange}) {
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input
-        id="name"
-        value={name}
-        onChange={event => setName(event.target.value)}
-      />
+      <input id="name" value={name} onChange={onNameChange} />
     </div>
   )
 }
@@ -28,23 +23,24 @@ function FavoriteAnimal({animal, onAnimalChange}) {
 }
 
 // 🐨 uncomment this
-function Display({animal}) {
-  return <div>{`Your favorite animal is: ${animal}!`}</div>
+function Display({name, animal}) {
+  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
 }
 
 function App() {
   // 🐨 add a useState for the animal
+  const [name, setName] = React.useState('')
   const [animal, setAnimal] = React.useState('')
   return (
     <form>
-      <Name />
+      <Name name={name} onNameChange={event => setName(event.target.value)} />
       {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
       <FavoriteAnimal
         animal={animal}
         onAnimalChange={event => setAnimal(event.target.value)}
       />
       {/* 🐨 pass the animal prop here */}
-      <Display animal={animal} />
+      <Display name={name} animal={animal} />
     </form>
   )
 }
